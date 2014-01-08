abc6aff02e3dfc7c350874f9d4ea23a82dd21464
Jubakuba <Jubakuba@gmail.com>
2014-01-08 02:56:19 -0700

    Frameworks: Favorite Contact Tile (1/2)

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -2649,6 +2649,12 @@ public final class Settings {
         public static final String POINTER_LOCATION = "pointer_location";
 
         /**
+         * Contact strings for infinite toggles
+         * @hide
+         */
+        public static final String TILE_CONTACT_ACTIONS = "tile_contact_actions";
+
+        /**
          * Show icon when stylus is used?
          * 0 = no
          * 1 = yes

9c53c5d7ae7efb520afa19a6a9a18a2b6a6ad674
aikons <dugujianke@live.com>
2014-01-17 12:30:36 +0800

    QS: add flip animation to Fast charge tile and Custom tile

--- packages/SystemUI/src/com/android/systemui/quicksettings/CustomTile.java
+++ packages/SystemUI/src/com/android/systemui/quicksettings/CustomTile.java
@@ -77,6 +77,9 @@ public class CustomTile extends QuickSettingsTile {
             @Override
             public void onClick(View v) {
                 performClickAction();
+                if (isFlipTilesEnabled()) {
+                    flipTile(0);
+                }
             }
         };
         mOnLongClick = new View.OnLongClickListener() {

c9d8695114a3a8862d55480eff4f08d95190abb6
scott <scott@ubuntu>
2014-02-06 00:28:32 -0500

    Fix large recents and remove dual pane option

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4977,11 +4977,6 @@ public final class Settings {
         /**
          * @hide
          */
-        public static final String DUAL_PANE_PREFS = "dual_pane_prefs";
-
-        /**
-         * @hide
-         */
         public static final String LARGE_RECENT_THUMBS = "large_recent_thumbs";
 
         /**

706250beb2a786ef4a3008f1ecc0751b2badd809
Clark Scheff <clark@cyngn.com>
2014-04-25 18:29:54 -0700

    Reduce memory leaks by removing all views.
    Change-Id: I1c7d3647ae0d40aa214108ea22e007e3cb3191b9
    
    Conflicts:
    	packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
@@ -4454,6 +4454,8 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
         mStatusBarContainer.removeAllViews();
         mStatusBarContainer.clearDisappearingChildren();
 
+        removeAllViews(mStatusBarWindow);
+
         // extract icons from the soon-to-be recreated viewgroup.
         int nIcons = mStatusIcons.getChildCount();
         ArrayList<StatusBarIcon> icons = new ArrayList<StatusBarIcon>(nIcons);
@@ -4505,6 +4507,17 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
         mRecreating = false;
 
         updateHalo();
+    }
+
+    private void removeAllViews(ViewGroup parent) {
+        int N = parent.getChildCount();
+        for (int i = 0; i < N; i++) {
+            View child = parent.getChildAt(i);
+            if (child instanceof ViewGroup) {
+                removeAllViews((ViewGroup) child);
+            }
+        }
+        parent.removeAllViews();
     }
 
     /**

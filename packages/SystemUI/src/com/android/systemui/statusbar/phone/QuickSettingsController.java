SlimRoms/frameworks_base

fb: add remote view tile (1/2)
Thanks CM for the base for it....just adapted it to our QS

Change-Id: Ied1dc1191e8aacb579046413dc2b12b1a634367f

packages/SystemUI/src/com/android/systemui/statusbar/phone/QuickSettingsController.java
@@ -54,6 +54,7 @@ import static com.android.internal.util.slim.QSConstants.TILE_VOLUME;
 import static com.android.internal.util.slim.QSConstants.TILE_WIFI;
 import static com.android.internal.util.slim.QSConstants.TILE_WIFIAP;
 import static com.android.internal.util.slim.QSConstants.TILE_REBOOT;
+import static com.android.internal.util.slim.QSConstants.TILE_REMOTEDISPLAY;
 
 import android.content.BroadcastReceiver;
 import android.content.ContentResolver;
@@ -104,6 +105,7 @@ import com.android.systemui.quicksettings.TorchTile;
 import com.android.systemui.quicksettings.UsbTetherTile;
 import com.android.systemui.quicksettings.UserTile;
 import com.android.systemui.quicksettings.VolumeTile;
+import com.android.systemui.quicksettings.RemoteDisplayTile;
 import com.android.systemui.quicksettings.WiFiTile;
 import com.android.systemui.quicksettings.WifiAPTile;
 import com.android.systemui.quicksettings.RebootTile;
@@ -295,6 +297,9 @@ public class QuickSettingsController {
             } else if (tile.equals(TILE_USBTETHER)
                     && DeviceUtils.deviceSupportsUsbTether(mContext)) {
                 qs = new UsbTetherTile(mContext, this);
+            } else if (tile.equals(TILE_REMOTEDISPLAY)
+                    && DeviceUtils.deviceSupportsRemoteDisplay(mContext)) {
+                qs = new RemoteDisplayTile(mContext, this);
             }
 
             if (qs != null) {

Jaiglissechef-i9100/android_frameworks_base

Signed-off-by: scott <scott@ubuntu>
Conflicts:
	core/java/com/android/internal/util/beanstalk/DeviceUtils.java
	packages/SystemUI/src/com/android/systemui/statusbar/phone/QuickSettingsController.java

packages/SystemUI/src/com/android/systemui/statusbar/phone/QuickSettingsController.java
@@ -62,6 +62,7 @@ import static com.android.internal.util.beanstalk.QSConstants.TILE_REBOOT;
 import static com.android.internal.util.beanstalk.QSConstants.TILE_INTERNALMEMORY;
 import static com.android.internal.util.beanstalk.QSConstants.TILE_FCHARGE;
 import static com.android.internal.util.beanstalk.QSConstants.TILE_ONTHEGO;
+import static com.android.internal.util.beanstalk.QSConstants.TILE_REMOTEDISPLAY;
 
 import android.app.Activity;
 import android.app.ActivityManagerNative;
@@ -122,6 +123,7 @@ import com.android.systemui.quicksettings.TorchTile;
 import com.android.systemui.quicksettings.UsbTetherTile;
 import com.android.systemui.quicksettings.UserTile;
 import com.android.systemui.quicksettings.VolumeTile;
+import com.android.systemui.quicksettings.RemoteDisplayTile;
 import com.android.systemui.quicksettings.WiFiTile;
 import com.android.systemui.quicksettings.WifiAPTile;
 import com.android.systemui.quicksettings.RebootTile;
@@ -336,6 +338,9 @@ public class QuickSettingsController {
             } else if (tile.equals(TILE_USBTETHER)
                     && DeviceUtils.deviceSupportsUsbTether(mContext)) {
                 qs = new UsbTetherTile(mContext, this);
+            } else if (tile.equals(TILE_REMOTEDISPLAY)
+                    && DeviceUtils.deviceSupportsRemoteDisplay(mContext)) {
+                qs = new RemoteDisplayTile(mContext, this);
             }
 
             if (qs != null) {